730ab5000aa3b3c028877d0e66ed534f99fdb824
dravonosec <dravonosec@yandex.ru>
2021-08-30 12:34:22 +0500

    Рабочая программа

--- src/main/java/com/renue/exam/ExamApplication.java
+++ src/main/java/com/renue/exam/ExamApplication.java
@@ -1,68 +1,69 @@
 package com.renue.exam;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
+
+
 @SpringBootApplication
 public class ExamApplication implements CommandLineRunner {
 
-    private static Logger LOG  = LoggerFactory
-            .getLogger(ExamApplication.class);
+    @Value("${config.columns}")
+    private Integer columns;
 
     public static void main(String[] args) {
-        LOG.info("STARTING THE APPLICATION");
         SpringApplication.run(ExamApplication.class, args);
-        LOG.info("APPLICATION FINISHED");
     }
 
     @Override
     public void run(String... args) throws Exception {
-        LOG.info ("EXECUTING: command line runner");
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите строку: ");
-        Runtime rt = Runtime.getRuntime();
         String filter = scanner.nextLine();
+        scanner.close();
         long m = System.currentTimeMillis();
-        readAllLines(filter);
-        for (int i = 0; i < args.length; i++)
-            LOG.info ("args[{}]: {}", i, args[i]);
-        System.out.println("Время выполнения программы: " + (System.currentTimeMillis() - m));
+        int count = getNumberOfMatches(filter);
 
-        System.out.println("Free Memory:" + rt.freeMemory());
-        System.out.println("Total Memory:" + rt.freeMemory());
-        System.out.println("Max Memory:" + rt.maxMemory());
+        System.out.println("Количество совпадений:" + count);
+        System.out.println("Время выполнения программы: " + (System.currentTimeMillis() - m) + " ms");
     }
     
-    public void readAllLines (String filter) throws IOException {
-        FileReader fileReader = new FileReader("src/main/resources/airports.dat");
+    public int getNumberOfMatches (String filter) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        FileReader fileReader = new FileReader("airports.dat");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         String line;
         int counter = 0;
-        Map<String, String> map=new TreeMap<String, String>();
+
+        Map<String, String> map = new TreeMap<>();
         while((line=bufferedReader.readLine())!=null){
-            if (getField(line).contains(filter)) {
-                map.put(getField(line), line);
+            String element = getField(line, columns);
+            if (element.contains(filter)) {
+                map.put(element, line);
                 counter++;
             }
         }
+
         bufferedReader.close();
-        FileWriter writer = new FileWriter("sorted_numbers.txt");
+        System.out.println("Результат работы:");
         for(String val : map.values()){
-            writer.write(val);
-            writer.write('\n');
+            System.out.println(val);
         }
-        writer.close();
+        return counter;
     }
-    private static String getField(String line) {
-        return line.split(",")[1];
+
+    private static String getField(String line, int columnNumber) {
+        return line.split(",")[columnNumber];
     }
 }
